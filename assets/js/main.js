697c7a842c661d34aefe6e430e3450959911fa6c
Mohamed Ait Si Mhand <mohamedaitsimhand@gmail.com>
2021-03-14 15:00:00 +0100

    Date inputes are ready

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,11 +2,8 @@ $(() => {
   $("#toggle-sidebar").change(() => {
     $("#sidebar").toggleClass("is-active");
   });
-//   $("input[type=date]").on("input", function () {
-//     if ($(this).val().length > 0) {
-//       $(this).addClass("full");
-//     } else {
-//       $(this).removeClass("full");
-//     }
-//   });
+  $("input[type=date]").on("input", function () {
+    if ($(this).val().length > 0) $(this).addClass("empty");
+    else $(this).removeClass("empty");
+  });
 });
